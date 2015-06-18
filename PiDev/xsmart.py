540049a5514a85035e3bfd5ffd3ca0655f8743a3
katta <katta@igw.tuwien.ac.at>
2015-06-18 13:30:47 +0000

    picture advancement implemented

--- PiDev/xsmart.py
+++ PiDev/xsmart.py
@@ -5,7 +5,7 @@ import PIL
 from PIL import Image
 import RPi.GPIO as GPIO
 
-import sys, os
+import sys, os, random
 
 # local imports
 import constants as con
@@ -13,8 +13,16 @@ import constants as con
 # global variables
 screen = None
 mode = con.STORYMODE
+picPool = []
+storyShown = False
 
 # implementation
+
+
+# get an array of all available pictures
+def get_all_pictures():
+	return os.listdir(con.PICS)
+	
 
 # this declutters the main function
 def setup():
@@ -34,6 +42,10 @@ def setup():
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(con.ADVANCEBUTTON, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 	GPIO.setup(con.MODEBUTTON, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+	
+	# setup picture pool
+	global picPool
+	picPool = get_all_pictures()
 	
 	
 
@@ -56,7 +68,7 @@ def test_for_input():
 			if mode == con.VIDEOMODE:
 				print "here we would advance to the next video"
 			elif mode == con.STORYMODE:
-				print "here we would advance to the next picture"
+				storytime()
 			toggle = False
 					
 	for event in pygame.event.get():
@@ -65,15 +77,33 @@ def test_for_input():
 				print "Goodbye."
 				exit()
 	
+# suggest picture pause
+def no_pictures():
+	global screen
+	screen.fill(con.BACKGROUNDCOLOUR)
 	
+	
+# shows a random picture in the current picPool
+def storytime():
+	global picPool, screen
+	if len(picPool) > 0:
+		pic =  random.choice(picPool)	
+		screen.blit(pygame.image.load(con.PICS + pic), (0,0))
+		pygame.display.flip()	
+		picPool.remove(pic)
+	else: 
+		print "picPool empty"
+		
 			
 # main function
 def main():
+	global storyShown
 	setup()	
 	# for now only a way to exit
 	while True:
-		if mode == con.STORYMODE:
-			print "storytimes"
+		if mode == con.STORYMODE and not storyShown:
+			storytime()
+			storyShown = True
 		test_for_input()
 
 main()
