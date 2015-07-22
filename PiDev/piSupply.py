977da160779d9b685da9c599d868d7aa8e2db10f
katta <katta@igw.tuwien.ac.at>
2015-07-22 15:07:29 +0000

    use a while loop instead of interrupts

--- PiDev/piSupply.py
+++ PiDev/piSupply.py
@@ -1,17 +1,12 @@
-# Import the modules to send commands to the system and access GPIO pins
-from subprocess import call
-import RPi.GPIO as gpio
- 
-# Define a function to keep script running
-def loop():
-    raw_input()
- 
-# Define a function to run when an interrupt is called
-def shutdown(pin):
-    call('halt', shell=False)
- 
-gpio.setmode(gpio.BCM) # Set pin numbering to board numbering
-gpio.setup(4, gpio.IN) # Set up pin 7 as an input
-gpio.add_event_detect(4, gpio.RISING, callback=shutdown, bouncetime=200) # Set up an interrupt to look for button presses
- 
-loop() # Run the loop function to keep script running
+# Import the libraries to use time delays, send os commands and access GPIO pins
+import RPi.GPIO as GPIO
+import time
+import os
+
+GPIO.setmode(GPIO.BCM) # Set pin numbering to GPIO numbering
+GPIO.setup(4, GPIO.IN) # Setup GPIO 4 as an input
+while True: # Setup a while loop to wait for a button press
+    if(GPIO.input(4)): # Setup an if loop to run a shutdown command when button press sensed
+        os.system("sudo shutdown -h now") # Send shutdown command to os
+        break
+    time.sleep(0.3) # Allow a sleep time of 1 second to reduce CPU usage
